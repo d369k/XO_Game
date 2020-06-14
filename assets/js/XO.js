73604cf2f9ef064f98f9600031f8fef9fb69724d
~|Daniel Ill! <daniel.malekzadeh.h@gmail.com>
2020-06-14 05:34:19 +0430

    add define winner function

--- assets/js/XO.js
+++ assets/js/XO.js
@@ -44,6 +44,38 @@ $(document).ready(function() {
         player_text = (current_player == "X") ? "Player : (O)" : "Player : (X)";
         $(".textC").text(player_text);
     }
-    
+
+    // Define winner function
+    function check_winner() {
+        if (game_board[1] == game_board[2] and game_board[2] == game_board[3]) { // 1 2 3 (win)
+            return true;
+        } else if (game_board[4] == game_board[5] and game_board[5] == game_board[6]) { // 4 5 6 (win)
+            return true;
+        } else if (game_board[7] == game_board[8] and game_board[8] == game_board[9]) { // 7 8 9 (win)
+            return true;
+        } else if (game_board[1] == game_board[4] and game_board[4] == game_board[7]) { // 1 4 7 (win)
+            return true;
+        } else if (game_board[2] == game_board[5] and game_board[5] == game_board[8]) { // 2 5 8 (win)
+            return true;
+        } else if (game_board[3] == game_board[6] and game_board[6] == game_board[9]) { // 3 6 9 (win)
+            return true;
+        } else if (game_board[1] == game_board[5] and game_board[5] == game_board[9]) { // 1 5 9 (win)
+            return true;
+        } else if (game_board[3] == game_board[5] and game_board[5] == game_board[7]) { // 3 5 7 (win)
+            return true;
+        } else if (game_board[1] != '1'
+            and game_board[2] != '2'
+            and game_board[3] != '3'
+            and game_board[4] != '4'
+            and game_board[5] != '5'
+            and game_board[6] != '6'
+            and game_board[7] != '7'
+            and game_board[8] != '8'
+            and game_board[9] != '9') { // game did not win
+            return -1;
+        } else {
+            return false;
+        }
+    }
 
 });
